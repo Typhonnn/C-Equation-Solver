81b0ea69e8a0c03ce1d8052386d5dadda654a1e6
Tal <mrtal1994@gmail.com>
2019-12-21 11:47:50 +0200

    start work on calculations

--- src/main.c
+++ src/main.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include "allEquations.c"
+#include "Solver.c"
 
 #define EQ_SIZE 80
 #define MAX_EQUATIONS 3
 
-void printEquation(Equation *tempEq) {
-	int i;
-	for (i = 0; i < tempEq->count; i++) {
-		printf("Hi %.3f ", *(tempEq->A + i));
+void printAllEquations(AllEquations *allEq) {
+	int i, j;
+	for (i = 0; i < allEq->count; i++) {
+		for (j = 0; j < allEq->count; j++) {
+			printf("%.3f ", allEq->eqArr[i]->A[j]);
+		}
 	}
 }
 
@@ -20,10 +23,14 @@ void* setEquation(AllEquations *allEq, char *eqStr) {
 	int i = 0;
 	char var[MAX_EQUATIONS];
 	Equation *tempEq = malloc(sizeof(Equation));
+	if (tempEq == NULL) {
+		printf("ERROR! COULD NOT MALLOC tempEq!");
+		return NULL;
+	}
 	tempEq->count = allEq->count;
 	tempEq->A = malloc(allEq->count * sizeof(float));
-	if (tempEq->A == NULL || tempEq == NULL) {
-		printf("FAILED TO MALLOC EQUATION!\n");
+	if (tempEq->A == NULL) {
+		printf("FAILED TO MALLOC tempEq->A!\n");
 		return NULL;
 	}
 	strcpy(eqCpy, eqStr);
@@ -39,7 +46,6 @@ void* setEquation(AllEquations *allEq, char *eqStr) {
 		}
 	}
 	tempEq->B = num;
-	printEquation(tempEq);
 	return tempEq;
 }
 
@@ -49,25 +55,59 @@ void setAllEquations(AllEquations *allEq) {
 	for (i = 0; i < (allEq->count); ++i) {
 		printf("Enter equation %d: ", i + 1);
 		scanf("%s", eqStr);
-		*((allEq->eqArr) + i) = setEquation(allEq, eqStr);
-		if (((allEq->eqArr) + i) == NULL) {
-			return;
-		}
+		allEq->eqArr[i] = setEquation(allEq, eqStr);
 		memset(eqStr, 0, EQ_SIZE);
 	}
+}
+
+void setSolverMatrix(Solver *solver, AllEquations *allEq) {
+	int i;
+	for (i = 0; i < solver->count; ++i) {
+		solver->A_Mat[i] = allEq->eqArr[i]->A;
+		solver->B_Vec[i] = allEq->eqArr[i]->B;
+	}
+}
+
+void setSolver(Solver *solver, AllEquations *allEq) {
+	solver->count = allEq->count;
+	solver->A_Mat = malloc(solver->count * sizeof(float*));
+	if (solver->A_Mat == NULL) {
+		printf("ERROR! COULD NOT MALLOC solver->A_Mat!");
+		return;
+	}
+	solver->B_Vec = malloc(solver->count * sizeof(float*));
+	if (solver->B_Vec == NULL) {
+		printf("ERROR! COULD NOT MALLOC solver->B_Vec!");
+		return;
+	}
+	setSolverMatrix(solver, allEq);
+}
+
+void calDetrmin(Solver *solver) {
+
+
 }
 
 int main() {
 	AllEquations *allEq = malloc(sizeof(AllEquations));
 	if (allEq == NULL) {
+		printf("ERROR! COULD NOT MALLOC AllEquations!");
 		return 0;
 	}
 	printf("Number of equations: ");
 	scanf("%d", &(allEq->count));
 	allEq->eqArr = malloc(allEq->count * sizeof(Equation*));
 	if ((allEq->eqArr) == NULL) {
+		printf("ERROR! COULD NOT MALLOC eqArr!");
 		return 0;
 	}
 	setAllEquations(allEq);
+	Solver *solver = malloc(sizeof(Solver));
+	if (solver == NULL) {
+		printf("ERROR! COULD NOT MALLOC SOLVER!");
+		return 0;
+	}
+	setSolver(solver, allEq);
+	calDetrmin(solver);
 	return 0;
 }
